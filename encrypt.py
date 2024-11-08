1f0be1e29123d5c8d56a0ac1f42b4a0d859b54ad
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-11-07 20:54:11 -0400

    Add encrypt and decrypt functions that take the text and encrypt/decrypt it with the key

--- encrypt.py
+++ encrypt.py
@@ -1,3 +1,32 @@
+def encrypt(text, key):
+    encrypted_text = ""
+    key_length = len(key)
+
+    #Desplazar cada carácter con el valor de la clave correspondiente
+    for i, char in enumerate(text):
+        #Obtener valor ASCII de la clave
+        byte = ord(key[i % key_length])  
+        #Desplazamiento del carácter
+        encrypted = chr(ord(char) + byte) 
+        encrypted_text += encrypted
+
+    return encrypted_text
+
+def decrypt(encrypted_text, key):
+    decrypted_text = ""
+    length = len(key)
+
+    #Desplazar en sentido contrario
+    for i, char in enumerate(encrypted_text):
+        #Obtener valor ASCII de la clave
+        byte = ord(key[i % length])  
+        #Desplazamiento inverso
+        decrypted = chr(ord(char) - byte) 
+        decrypted_text += decrypted
+
+    return decrypted_text
+
+
 def read_text_from_file(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
